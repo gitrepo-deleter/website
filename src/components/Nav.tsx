96a5a878e15e57e0dbdf832fdb0eb1ecbc1ad360
Bashamega <adambashaahmednaji@gmail.com>
2024-08-29 12:07:42 +0300

    Title

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,7 @@
 export function Nav(){
     return(
-        <nav className=" flex justify-between p-5 shadow-lg">
-            <h1>Git Repo Deleter</h1>
+        <nav className=" flex justify-between p-5 shadow-lg items-center">
+            <h1 className=" text-2xl font-mono">Git Repo Deleter</h1>
             <button className="bg-[#50727B] hover:bg-[#425a61] px-5 py-2 rounded">Sign In</button>
         </nav>
     )
