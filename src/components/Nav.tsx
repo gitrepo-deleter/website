e352704eb3c649872c619a5cc170c3c4d88528d5
Bashamega <adambashaahmednaji@gmail.com>
2024-08-29 12:06:36 +0300

    Nav bar

---
+++ src/components/Nav.tsx
@@ -0,0 +1,8 @@
+export function Nav(){
+    return(
+        <nav className=" flex justify-between p-5 shadow-lg">
+            <h1>Git Repo Deleter</h1>
+            <button className="bg-[#50727B] hover:bg-[#425a61] px-5 py-2 rounded">Sign In</button>
+        </nav>
+    )
+}
